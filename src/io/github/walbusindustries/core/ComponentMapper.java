b881f86853c30de88d85e4129a38129b4b466b66
Sandro <sandro.delrivo@gmail.com>
2016-03-10 22:06:45 -0800

    Added javadoc to engine class. Removed entity class. ADded methods to Engine.

--- src/io/github/walbusindustries/core/ComponentMapper.java
+++ src/io/github/walbusindustries/core/ComponentMapper.java
@@ -1,5 +1,10 @@
 package io.github.walbusindustries.core;
 
+/**
+ * @author WalbusIndustries
+ * @version 0.1
+ * @since 2016-03-10
+ */
 public class ComponentMapper {
 
 }
--- src/io/github/walbusindustries/core/Engine.java
+++ src/io/github/walbusindustries/core/Engine.java
@@ -2,16 +2,39 @@ package io.github.walbusindustries.core;
 
 import java.util.Vector;
 
+/**
+ * @author WalbusIndustries
+ * @version 0.1
+ * @since 2016-03-10
+ */
 public class Engine {
 
+	// Vector containing all of the entity systems in the project.
+	// All entity systems that the program will run will need to be added to
+	// this list.
 	private Vector<EntitySystem> entitySystems;
 
 	int eCount = 0;
 
-	public Engine() {
+	/**
+	 * Method to access the entity systems of the engine.
+	 * 
+	 * @return The vector containing all of the entity systems.
+	 */
+	public Vector<EntitySystem> getEntitySystems() {
 
+		return entitySystems;
 	}
 
+	/**
+	 * Creates entity
+	 * <p>
+	 * A unique id is giving to the entity (an entity being an id represented by
+	 * an integer). This allows the system to manage the entity. Entity
+	 * assignments start at 0 and increment by one after every assignment.
+	 * 
+	 * @return A unique id.
+	 */
 	public int create() {
 
 		int e = eCount;
@@ -21,9 +44,27 @@ public class Engine {
 
 	}
 
+	/**
+	 * Add an entity system to the engine
+	 * <p>
+	 * This will add the entity system to the engine.
+	 * 
+	 * @param es
+	 *            - Entity System
+	 */
 	public void addSystem(EntitySystem es) {
 
 		entitySystems.add(es);
+
+	}
+
+	/**
+	 * Updates all of the entity systems.
+	 * <p>
+	 * This engine is designed to be used within any game engine so this update
+	 * method must be called during whenever the game updates its logic.
+	 */
+	public void update() {
 
 	}
 
