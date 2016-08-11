6f62a6848e09d7a7c81018a9a82b4547c7bcf2d5
Sandro <sandro.delrivo@gmail.com>
2016-08-11 12:25:05 -0700

    Added Jar

--- src/io/github/walbusindustries/core/Component.java
+++ src/io/github/walbusindustries/core/Component.java
@@ -2,7 +2,7 @@ package io.github.walbusindustries.core;
 
 /**
  * @author WalbusIndustries
- * @version 0.1
+ * @version 0.2
  * @since 2016-03-10
  */
 public abstract class Component {
--- src/io/github/walbusindustries/core/EntitySystem.java
+++ src/io/github/walbusindustries/core/EntitySystem.java
@@ -6,7 +6,7 @@ import java.util.Set;
 
 /**
  * @author WalbusIndustries
- * @version 0.1
+ * @version 0.2
  * @since 2016-03-10
  */
 public abstract class EntitySystem {
